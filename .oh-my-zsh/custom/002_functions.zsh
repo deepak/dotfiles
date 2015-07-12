fef537e4416bb61da45b93ae3041d9ef9f245721
Deepak Kannan <kannan.deepak@gmail.com>
2015-07-12 23:05:07 +0530

    extract dotfiles everytime upgrade script is run
    I run `update_lappy` pretty frequently.
    
    call `github_dotfiles` also at the end there,
    so that the github dotfiles repo has
    a good chance of being in sync.

--- .oh-my-zsh/custom/002_functions.zsh
+++ .oh-my-zsh/custom/002_functions.zsh
@@ -1,18 +1,32 @@
+function github_dotfiles() {
+    local repo_dir=$(ruby -e "puts File.expand_path(ARGV[0])" "Code/forks/deepak-dotfiles")
+    local oh_my_zsh_dir="$repo_dir/.oh-my-zsh"
+    echo $repo_dir
+    echo $oh_my_zsh_dir
+    mkdir -p ${oh_my_zsh_dir}
+
+    cp ~/.zshrc ${repo_dir}
+    cp -r ~/.oh-my-zsh/custom ${oh_my_zsh_dir}
+    cd ${repo_dir}
+    git status
+}
+
 function update_lappy() {
     brew update
     brew upgrade
     brew cleanup
     upgrade_oh_my_zsh
+    github_dotfiles
 }
 
 function mirror_work_heroku_db() {
-    cw
+    cw # alias for cd <word-dir>
     heroku pg:backups capture
     curl -o latest.dump `heroku pg:backups public-url`
     pg_restore --verbose --clean --no-acl --no-owner -h localhost -U deepak -d emami_backup latest.dump
 }
 
-# eg. crun hello,
+# eg. crun hello
 # will execute:
 # cc -Wall    hello.c   -o hello
 function crun() {
@@ -26,17 +40,4 @@ function crun() {
 # TODO: PR pending at https://github.com/caskroom/homebrew-cask/pull/12563
 function manu() {
     open "man:$1"
-}
-
-function github_dotfiles() {
-    local repo_dir=$(ruby -e "puts File.expand_path(ARGV[0])" "Code/forks/deepak-dotfiles")
-    local oh_my_zsh_dir="$repo_dir/.oh-my-zsh"
-    echo $repo_dir
-    echo $oh_my_zsh_dir
-    mkdir -p ${oh_my_zsh_dir}
-
-    cp ~/.zshrc ${repo_dir}
-    cp -r ~/.oh-my-zsh/custom ${oh_my_zsh_dir}
-    cd ${repo_dir}
-    git status
 }
